3d1f358c9620e046438d76bc7458a24d46889212
fajarfaz <fajarfaz@gmail.com>
2021-05-08 14:34:39 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,14 +23,14 @@
         <label class="font-semibold text-2xl leading-none text-gray-800">Sign in </label>
         <label class="font-base  text-lg leading-tight">Sign in and start recording your attendance.</label>
         <div class="relative flex w-full flex-wrap items-stretch my-1">
-            <span class="z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-10 pl-3 py-4 md:py-3">
+            <span class="z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-10 pl-3 flex items-center md:py-3">
                 <img src="{{ asset('/image/name.svg')}}" alt="username" class="w-6 opacity-50" >
             </span>
             <x-jet-input type="email" name="email" :value="old('email')" required autofocus placeholder="{{ __('Email') }}" class="px-3 py-2 placeholder-gray-400 text-gray-700 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pl-12 text-lg hover:border-blue-400 duration-1000"/>
         </div>     
 
         <div class="relative flex w-full flex-wrap items-stretch mb-3">
-            <span class="z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-4 md:py-3">
+            <span class="z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 flex items-center md:py-3">
                 <img src="{{ asset('/image/padlock.svg')}}" alt="lock" class="w-6 opacity-50" >
             </span>
             <x-jet-input  type="password" id="password" name="password" autocomplete="current-password" required autofocus placeholder="{{ __('Password') }}" class="px-3 py-2 placeholder-gray-400 text-gray-700 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pl-12 text-lg hover:border-blue-400 duration-1000"/>
