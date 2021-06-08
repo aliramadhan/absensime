23c61150afe881e8913a7fe79f087fc36bebcae1
siuyan <aliachmadramadhan@gmail.com>
2021-06-08 15:43:07 +0700

    Update request-admin.blade.php

--- resources/views/livewire/Admin/request-admin.blade.php
+++ resources/views/livewire/Admin/request-admin.blade.php
@@ -1,10 +1,11 @@
 <div class="bg-white shadow">
-<div class="flex justify-between items-center max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 ">
-     <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
-        {{ __('Working Request') }}
-    </h2>
-  
-</div>
+    <div class="flex justify-between items-center max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 ">
+         <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
+            {{ __('Working Request') }}
+        </h2>
+        
+    </div>
+
 </div>
 
 <div class="py-12">
