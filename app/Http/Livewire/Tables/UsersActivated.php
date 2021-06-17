eeee7b5988c8c1e0d1803f812542fde39fc54988
aliramadhan <yuukikimisaki@gmail.com>
2021-06-17 11:22:37 -0700

    Update UsersActivated.php

--- app/Http/Livewire/Tables/UsersActivated.php
+++ app/Http/Livewire/Tables/UsersActivated.php
@@ -13,47 +13,47 @@ use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
 
 class UsersActivated extends LivewireDatatable
 {
-	public $user;
+    public $user;
     public function builder()
     {
-	    return User::where('role','Employee')->orderBy('is_active','asc')->orderBy('updated_at','asc');
+        return User::where('roles','Employee');
     }
 
     public function columns()
-    {	
-    	return [
-	    	Column::name('name')
-	    		->label('Employee Name'),
-	    	Column::name('role')
-	    		->label('Role'),
+    {   
+        return [
+            Column::name('name')
+                ->label('Employee Name'),
+            Column::name('role')
+                ->label('Role'),
             Column::name('division')
                 ->label('Division'),
             Column::name('leave_count')
                 ->label('Remain Annual Leave')->editable(),
-	    	Column::callback(['is_active'], function ($is_active) {
-	        	if ($is_active == 1) {
-	        		return 'Active';
-	        	}
-		        return 'non-Active';
-	        })->label('Status'),
-	        Column::callback(['updated_at','is_active'], function ($updated_at,$is_active) {
-	        	if ($is_active == 1) {
-	        		return '-';
-	        	}
-	        	$time = Carbon::parse($updated_at)->format('H:i, d F Y');
-		        return $time;
-	        })->label('Lock Time'),
+            Column::callback(['is_active'], function ($is_active) {
+                if ($is_active == 1) {
+                    return 'Active';
+                }
+                return 'non-Active';
+            })->label('Status'),
+            Column::callback(['updated_at','is_active'], function ($updated_at,$is_active) {
+                if ($is_active == 1) {
+                    return '-';
+                }
+                $time = Carbon::parse($updated_at)->format('H:i, d F Y');
+                return $time;
+            })->label('Lock Time'),
             Column::callback(['id'], function ($id) {
-            	$user = User::find($id);
-            	if ($user->is_active == 0) {
-	                return view('livewire.Admin.table_actions.table-action-user-activated', ['id' => $id, 'user' => $user]);
-            	}
+                $user = User::find($id);
+                if ($user->is_active == 0) {
+                    return view('livewire.Admin.table_actions.table-action-user-activated', ['id' => $id, 'user' => $user]);
+                }
             })->label('Action')
-    	];
+        ];
     }
     public function activatedUser($user)
     {
-    	$user = User::find($user['id']);
+        $user = User::find($user['id']);
         if ($user == null) {
             session()->flash('failure', 'User not found.');
         }
