9f9acde1a44afd422044c2248bff7d0d2f0a30f6
fajarfaz <fajarfaz@gmail.com>
2021-07-22 14:13:39 +0700

    change title

--- resources/views/livewire/Admin/employee-presence.blade.php
+++ resources/views/livewire/Admin/employee-presence.blade.php
@@ -1,6 +1,6 @@
 <x-slot name="header">
     <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
-        {{ __('Employee Schedule') }}
+        {{ __('Attendance Report') }}
     </h2>
 </x-slot>
 
