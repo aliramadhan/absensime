8be0fa57c179ae8f3749060b6d19515a9c3cb223
fajarfaz <fajarfaz@gmail.com>
2021-08-10 11:35:10 +0700

    stop

--- resources/views/livewire/User/show_stop.blade.php
+++ resources/views/livewire/User/show_stop.blade.php
@@ -1,6 +1,6 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-  <div class="flex items-start justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+  <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
     <!--
       Background overlay, show/hide based on modal state.
 
@@ -26,47 +26,70 @@
         From: "opacity-100 translate-y-0 sm:scale-100"
         To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     -->
-
-    <audio  autoplay >
-      <source src="{{ asset('audio/sound.mp3') }}" type="audio/mp3" />
-    </audio>
-    
-    <div class="w-full inline-block align-bottom bg-white rounded-lg text-left  shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-        <div class="sm:flex sm:items-start">
-          <div class="mx-auto flex-shrink-0 flex md:flex-row flex-col items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+    @php
+      $detailsSchedule = $schedule->details->where('task',null)->sortBy('id');
+    @endphp
+    <div class="w-full overflow-hidden inline-block align-bottom bg-white rounded-lg text-left  shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+      <div class="bg-white px-4 pt-5 pb-2 sm:p-6 sm:pb-4">
+        <div class="sm:flex sm:items-start ">
+          <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
             <!-- Heroicon name: outline/exclamation -->
             <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
             </svg>
           </div>
-          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left grid gap-2 w-full">
-            <h3 class="text-xl leading-6 font-semibold text-gray-800" id="modal-title">
-              Confirmation to continue recording
-              
-            </h3>
-           
-            <div class="mt-2 grid flex-col ">
-              <p class="text-sm text-gray-500 tracking-wide leading-2 ">
-                Now your Working Hours have been fulfilled, are you sure to continue recording?<br><span class="text-red-600">This action cannot be undone </span>.
-              </p>
+          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left flex-col flex md:grid gap-2 w-full pb-2">
+            <div id="modal-title" class="flex-auto text-left border-b pb-1">
+              <h3 class="text-base md:text-lg leading-none font-medium text-gray-900" >
+                Stop Recording & Journaling <label class="hidden sm:inline-block">Recapitulation</label>
+              </h3>
+             <label class=" tracking-wide text-xs md:text-sm text-gray-800">You Have <label class="font-semibold">{{$detailsSchedule->count()}}</label> empty journal</label>       
             </div>
-        
+
+            <div class="overflow-y-auto max-h-72 pr-2 flex-auto border-b">     
+             
+               <div class="text-sm my-2  px-2 flex-col space-y-1">
+                @forelse($detailsSchedule as $details)
+                  <div class="flex space-x-4 items-center ">
+                  <label class="w-1">{{$loop->iteration}}.</label>
+                  <label class="flex-shrink-0 w-18">{{Carbon\Carbon::parse($details->started_at)->format('H:i')}} - {{Carbon\Carbon::parse($details->stoped_at)->format('H:i')}}</label>
+                  <input type="text" class="rounded-lg tracking-wide py-2 px-3 w-9/12 text-sm border-gray-200 bg-gray-200 focus:outline-none focus:bg-white" required placeholder="Fill your task/journal.." wire:model="detailsSchedule.{{ $loop->iteration }}.task">
+                </div>
+                
+                @empty
+
+                @endforelse
+             </div>
+            
+
+           
+            </div>
+
+            @if($now < Carbon\Carbon::parse($shift->time_out))  
+            <div class="flex-col space-y-1 text-left">      
+            <div class="flex space-x-2 text-gray-700 items-center pr-4 ">
+              <label class="font-base text-xs md:text-sm tracking-wide font-semibold">Reason</label>
+            <input type="text" wire:model="note" class="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline text-sm border-gray-200 bg-gray-200 focus:outline-none focus:bg-white tracking-wide" placeholder="Fill your reason.." required>
+            </div>
+             <label class="text-red-600 tracking-wide text-xs md:text-sm "><label class="font-semibold">Warning</label>: Stopped recording before <br class="sm:hidden inline-block">  shift over</label>
+             </div>   
+            @endif
+            <div class="border md:shadow-md rounded-lg text-white px-1 md:px-2 py-2">
+            <p class="text-xs md:text-sm text-gray-700 text-left">
+            Are you sure you want to stop recording?<br> This action cannot be undone.
+            </p>
+            </div>
           </div>
         </div>
       </div>
-
-        <div class="bg-gray-50 px-4 py-3 sm:px-6 items-center flex md:flex-row-reverse space-x-2">
-        <button wire:click="continueOn()" type="button" class="w-6/12 inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2   tracking-wider text-base font-medium text-gray-800 bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-          Continue
-        </button>
-
-        <button wire:click="stopOn()" type="button" class="w-6/12 inline-flex justify-center rounded-md border border-transparent shadow-sm px-8 py-2 bg-red-600 tracking-wider text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">
+      <div class="bg-gray-50 px-4 py-3 sm:px-6 items-center flex sm:flex-row-reverse space-x-2">
+        <button wire:click="stopOn()" type="button" class="w-6/12 inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm tracking-wider">
           Stop
         </button>
-        
+        <button wire:click="closeModal()" type="button" class="w-6/12 inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+          Cancel
+        </button>
       </div>
     </div>
   </div>
 </div>
-
