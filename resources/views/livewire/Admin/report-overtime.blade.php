89257e3e1d169acb960dcc1fc0ee439953c2aecc
fajarfaz <fajarfaz@gmail.com>
2021-08-27 13:49:48 +0700

    just title

--- resources/views/livewire/Admin/report-overtime.blade.php
+++ resources/views/livewire/Admin/report-overtime.blade.php
@@ -1,6 +1,6 @@
 <x-slot name="header">
     <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
-        {{ __('Attendance Report') }}
+        {{ __('Overtime Report') }}
     </h2>
 </x-slot>
 
