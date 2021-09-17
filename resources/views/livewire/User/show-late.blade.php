2cda4e7ff61a3a2c7f98974633a3fab6e5e8a489
fajarfaz <fajarfaz@gmail.com>
2021-09-17 13:54:00 +0700

    late add loading

--- resources/views/livewire/User/show-late.blade.php
+++ resources/views/livewire/User/show-late.blade.php
@@ -14,8 +14,8 @@
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <div class="">
                         <div class="mb-4">
-                            <label for="formNote" class="block text-gray-500 text-sm font-semibold mb-2">Reason late</label>
-                            <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline" id="formNote" wire:model="note">
+                            <label for="formNote" class="block text-gray-500 text-sm font-semibold mb-2">Your reason</label>
+                            <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 mb-2 text-gray-500 leading-tight focus:outline-none focus:shadow-outline" id="formNote" wire:model="note" required>
                             @error('note') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                     </div>
@@ -24,7 +24,8 @@
                 <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
                         <button wire:click.prevent="lateOn()" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-6 py-2 bg-blue-500 text-base leading-6 font-medium text-white shadow-sm hover:bg-blue-600 focus:outline-none focus:border-yellow-700 focus:shadow-outline-yellow transition ease-in-out duration-150 sm:text-sm sm:leading-5 items-center">
-                        <i class="fas fa-pause-circle mr-2 -ml-2"></i> Send
+                        <i class="fas fa-paper-plane mr-2 -ml-2" wire:loading.remove wire:target="lateOn"></i>
+                        <i class="fas fa-circle-notch animate-spin mr-2 -ml-2" wire:loading wire:target="lateOn"></i> Send and Start Record
                         </button>
                     </span>
                 </form>
