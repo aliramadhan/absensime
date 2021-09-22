af90a28d9260785bb58e5980d2d08a2145737960
aliramadhan <yuukikimisaki@gmail.com>
2021-09-22 08:19:33 +0700

    up

--- resources/views/livewire/User/dashboard.blade.php
+++ resources/views/livewire/User/dashboard.blade.php
@@ -468,11 +468,11 @@
                         $workhourDetail += $started_atDetail->diffInSeconds($stoped_atDetail);
                     }
                   @endphp
-                  @if($workhourDetail >= $limit_workhour && $schedule->status_stop == null && $workhourDetail <= ($limit_workhour + 1800) && $user->position != 'Project Manager')
+                  @if($workhourDetail >= $limit_workhour && $schedule->status_stop == null && $workhourDetail <= ($limit_workhour + 1800) && ($user->position != 'Project Manager' && $user->position != 'Junior PM'))
                     @include('livewire.User.show-confirm-stop')
-                  @elseif(($workhourDetail >= $limit_workhour+14400) && $schedule->status_stop == null && $workhourDetail <= ($limit_workhour + 16200) && $user->position == 'Project Manager')
+                  @elseif(($workhourDetail >= $limit_workhour+14400) && $schedule->status_stop == null && $workhourDetail <= ($limit_workhour + 16200) && ($user->position == 'Project Manager'  && $user->position == 'Junior PM'))
                     @include('livewire.User.show-confirm-stop')
-                  @elseif($workhourDetail >= $limit_workhour && $schedule->status_stop == null && $workhourDetail > ($limit_workhour + 1800) && $user->position != 'Project Manager')
+                  @elseif($workhourDetail >= $limit_workhour && $schedule->status_stop == null && $workhourDetail > ($limit_workhour + 1800) && ($user->position != 'Project Manager' && $user->position != 'Junior PM'))
                     @php
                       //update task and stop schedule
                       $detailSchedule = $schedule->details->sortByDesc('id')->first();
@@ -492,7 +492,7 @@
                           'status' => 'Done',
                       ]);
                     @endphp
-                  @elseif(($workhourDetail >= $limit_workhour+14400) && $schedule->status_stop == null && $workhourDetail > ($limit_workhour + 16200) && $user->position == 'Project Manager')
+                  @elseif(($workhourDetail >= $limit_workhour+14400) && $schedule->status_stop == null && $workhourDetail > ($limit_workhour + 16200) && ($user->position == 'Project Manager' && $user->position == 'Junior PM'))
                     @php
                       //update task and stop schedule
                       $detailSchedule = $schedule->details->sortByDesc('id')->first();
