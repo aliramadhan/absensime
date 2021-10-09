14abaa73dadf9acad1b463198c44db27d0f411e1
fajarfaz <fajarfaz@gmail.com>
2021-10-09 15:26:36 +0700

    guide file

---
+++ app/Http/Livewire/Guide.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Guide extends Component
+{
+	
+    public function render()
+    {
+        return view('livewire.User.guide');
+    }
+}
---
+++ resources/views/livewire/User/guide.blade.php
@@ -0,0 +1,11 @@
+<x-slot name="header">
+    <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
+        {{ __('Application Guide') }}
+    </h2>
+</x-slot>
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+       <iframe class="w-full bg-white h-screen rounded-lg shadow-md" src="https://docs.google.com/document/d/e/2PACX-1vTjjMpetIiE_9tb0_cREr4rhh8eZf5jmEE4Vz7vDmGPXFgffsMYB_Q3JaK_GMzguheZ5vD133_DJkVQ/pub?embedded=true"></iframe>
+    </div>
+</div>
