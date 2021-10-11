47ee05cbc3e4b4a74f963033669960058fdc9e90
aliramadhan <yuukikimisaki@gmail.com>
2021-10-11 10:18:22 +0700

    Update schedule-live.blade.php

--- resources/views/livewire/Admin/Schedule/schedule-live.blade.php
+++ resources/views/livewire/Admin/Schedule/schedule-live.blade.php
@@ -57,7 +57,7 @@
          <button wire:click="import()" class="bg-gradient-to-r from-green-500 to-green-600 duration-200 opacity-80 hover:opacity-100 md:px-6 px-4 md:py-2 py-3 flex items-center gap-2 text-lg font-semibold tracking-wider text-white rounded-xl shadow-md focus:outline-none "><i class="fas fa-file-import"></i> <span class="hidden md:block">Import </span></button>
          </div>
 
-         <div wire:loading wire:target="create,closeModal,importSchedule" class="overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center">
+         <div wire:loading wire:target="create,closeModal,importSchedule,create,import" class="overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center">
              <section class="h-full w-full border-box  transition-all duration-500 flex bg-gray-500 opacity-75"    >   
                 <div class="text-6xl text-white m-auto text-center">        
                    <i class="fas fa-circle-notch animate-spin"></i>
