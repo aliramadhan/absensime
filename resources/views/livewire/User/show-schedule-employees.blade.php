ff6a037037a4b6fe221cb0c26bf3db8c957a1a1e
aliramadhan <yuukikimisaki@gmail.com>
2021-11-05 10:57:53 +0700

    Update show-schedule-employees.blade.php

--- resources/views/livewire/User/show-schedule-employees.blade.php
+++ resources/views/livewire/User/show-schedule-employees.blade.php
@@ -143,6 +143,7 @@
             <div class="flex"> 
               {{$schedule->shift_name}}
               {{Carbon\Carbon::parse($schedule->shift->time_in)->format('H.i')}} - {{Carbon\Carbon::parse($schedule->shift->time_out)->format('H.i')}}
+              {{Carbon\Carbon::parse($schedule->started_at)->format('H.i')}} - {{Carbon\Carbon::parse($schedule->stoped_at)->format('H.i')}}
             </div>
           </td>
           @elseif($schedule != null && $schedule->status == 'No Record')
@@ -196,7 +197,8 @@
           <td class='hover:bg-green-500 px-1 py-2 text-center border border-white bg-green-400 font-semibold tracking-wide text-center text-sm text-white '>
             <div class="flex flex-col">
               <label class="text-base"> {{$schedule->shift_name}} </label>
-              <label class="font-normal"> {{Carbon\Carbon::parse($schedule->shift->time_in)->format('H.i')}} - {{Carbon\Carbon::parse($schedule->shift->time_out)->format('H.i')}} </label>
+              <label class="font-normal"> {{Carbon\Carbon::parse($schedule->shift->time_in)->format('H.i')}} - {{Carbon\Carbon::parse($schedule->shift->time_out)->format('H.i')}}
+              {{Carbon\Carbon::parse($schedule->started_at)->format('H.i')}} - {{Carbon\Carbon::parse($schedule->stoped_at)->format('H.i')}} </label>
             </div>
           </td>
           @elseif($schedule != null && $schedule->status == 'No Record')
